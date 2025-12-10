a211ebbb77a2ce64df602b788c0937b9a67a0f2d
Michal Kleiner <mk@011.nz>
2025-12-10 15:30:03 +1300

    Use newer version of the library, move code to src, add namespace, add types

--- src/Extensions/QrGeneratorExtension.php
+++ src/Extensions/QrGeneratorExtension.php
@@ -1,41 +1,48 @@
 <?php
 
+namespace Netwerkstatt\QrGenerator\Extensions;
+
+use Endroid\QrCode\Color\Color;
+use Endroid\QrCode\Encoding\Encoding;
 use Endroid\QrCode\ErrorCorrectionLevel;
 use Endroid\QrCode\QrCode;
+use Endroid\QrCode\RoundBlockSizeMode;
+use Endroid\QrCode\Writer\PngWriter;
+use SilverStripe\Control\Controller;
+use SilverStripe\Control\Director;
+use SilverStripe\Core\Extension;
+use SilverStripe\Forms\FieldList;
+use SilverStripe\Forms\HeaderField;
+use SilverStripe\Forms\LiteralField;
+use SilverStripe\View\Parsers\URLSegmentFilter;
 
 /**
  * Class QrGeneratorExtension
- *
- * @property SiteTree|QrGeneratorExtension $owner
  */
-class QrGeneratorExtension extends DataExtension
+class QrGeneratorExtension extends Extension
 {
     public function updateCMSFields(FieldList $fields)
     {
-        if (!$this->owner->isInDB()) {
+        if (!$this->getOwner()->isInDB()) {
             return;
         }
 
         $fields->addFieldsToTab('Root.QR', [
-            HeaderField::create('QRHeading', _t('QrGeneratorExtension.Title', 'QR-Code'), 2),
+            HeaderField::create('QRHeading', _t('QrGeneratorExtension.Title', 'QR Code'), 2),
             LiteralField::create('QRContent',
                 '<p><img src="data:image/png;base64,' . $this->getQRCodeBase64() . '" /></p>'),
             LiteralField::create('QRDownload', '<p><a href="' . $this->getQRCodeURL() . '" target="_blank">'
                 . _t('QrGeneratorExtension.Download', 'Download')
                 . '</a></p>')
         ]);
-
     }
 
-
     /**
-     * for inline images
+     * For inline images
      *
      * <img alt="Scan me" src="data:image/png;base64,$QRCodeBase64" />
-     *
-     * @return string
      */
-    public function getQRCodeBase64()
+    public function getQRCodeBase64(): string
     {
         return base64_encode($this->generateQRCode());
     }
@@ -58,18 +65,22 @@ class QrGeneratorExtension extends DataExtension
             return file_get_contents($filename);
         }
 
-        $qr = new QrCode($this->getQrCodeContent());
-        $qr->setSize(300)
-            ->setWriterByName('png')
-            ->setEncoding('UTF-8')
-            ->setErrorCorrectionLevel(ErrorCorrectionLevel::LOW)
-            ->setForegroundColor(['r' => 0, 'g' => 0, 'b' => 0])
-            ->setBackgroundColor(['r' => 255, 'g' => 255, 'b' => 255])
-            ->setValidateResult(false);
+        $qrCode = new QrCode(
+            data: $this->getQrCodeContent(),
+            encoding: new Encoding('UTF-8'),
+            errorCorrectionLevel: ErrorCorrectionLevel::Low,
+            size: 320,
+            margin: 5,
+            roundBlockSizeMode: RoundBlockSizeMode::Enlarge,
+            foregroundColor: new Color(0, 0, 0),
+            backgroundColor: new Color(255, 255, 255)
+        );
 
-        $qr->writeFile(ASSETS_PATH . $this->getQrCodeName());
+        $writer = new PngWriter();
+        $result = $writer->write($qrCode);
+        $result->saveToFile(ASSETS_PATH . $this->getQrCodeName());
 
-        return $qr->writeString();
+        return $result->getString();
     }
 
     /**
@@ -81,11 +92,11 @@ class QrGeneratorExtension extends DataExtension
      */
     private function getQrCodeName()
     {
-        $qrpath = '/qr/';
+        $qrPath = '/qr/';
 
-        //check if $path exists in assets
-        if (!is_dir(ASSETS_PATH . $qrpath)) {
-            mkdir(ASSETS_PATH . $qrpath);
+        // check if $path exists in assets
+        if (!is_dir(ASSETS_PATH . $qrPath)) {
+            mkdir(ASSETS_PATH . $qrPath);
         }
 
         $base = URLSegmentFilter::create()->filter(implode('-', [
@@ -96,28 +107,24 @@ class QrGeneratorExtension extends DataExtension
             substr(md5($this->getQrCodeContent()), 0, 5 )
         ]));
 
-        return $qrpath . $base . '.png';
+        return $qrPath . $base . '.png';
     }
 
     /**
-     * Uses absolute link as default.
+     * Use absolute link as default
      *
      * @todo: check if owner has a method to provide content. This might be useful for other types of codes,
      * e.g. for contact data, calendar data etc...
-     *
-     * @return mixed
      */
-    private function getQrCodeContent()
+    private function getQrCodeContent(): string
     {
-        return $this->owner->AbsoluteLink();
+        return $this->getOwner()->AbsoluteLink();
     }
 
     /**
      * URL for using in <img alt="Scan me" src="$QRCodeURL" />
-     *
-     * @return String
      */
-    public function getQRCodeURL()
+    public function getQRCodeURL(): string
     {
         $this->generateQRCode();
         return Controller::join_links(Director::baseURL(), ASSETS_DIR, $this->getQrCodeName());
